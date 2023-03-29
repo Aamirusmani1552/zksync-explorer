6546fd79ec57289730d27590dcd57ebc33c3d01d
Aamirusmani1552 <90125875+Aamirusmani1552@users.noreply.github.com>
2023-03-29 16:23:09 +0530

    added typescipt and tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className="bg-red-400 text-5xl">hello world</main>
+    </>
+  );
+}
